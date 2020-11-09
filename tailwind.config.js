c8709f390bbb85e2988ac3adf1c01c7c2617810f
Martino Roma <mroma@genesicom.com>
2020-11-09 11:54:19 +0100

    nuova font family

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,10 @@
 // tailwind.config.js
 module.exports = {
   theme: {
-    fontFamily: ["IBM Plex Sans", "sans serif"]
+    fontFamily: {
+      "sans": ["IBM Plex Sans", "sans serif"],
+      "serif": ['Georgia', 'serif'],
+    }
   },
   plugins: [require("@tailwindcss/typography")]
 };
