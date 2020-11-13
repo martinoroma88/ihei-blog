dae6b27b0fbfbad8899b67f339a94e3179bf3142
Martino Roma <mroma@genesicom.com>
2020-11-13 11:32:24 +0100

    scroll

--- components/Categories.vue
+++ components/Categories.vue
@@ -1,6 +1,6 @@
 <template>
 	<div class="overflow-y-hidden w-full">
-		<ul class="lg:space-y-3 font-sans flex lg:block lg:space-x-0 space-x-6 overflow-x-auto">
+		<ul id="categories" class="lg:space-y-3 font-sans flex lg:block lg:space-x-0 space-x-6 overflow-x-auto">
 			<li class="whitespace-no-wrap p-3 lg:p-0" v-if="main">
 				<n-link class="link" :to="main.url">{{main.title}}</n-link>
 			</li>
@@ -13,6 +13,14 @@
 
 <script>
 export default {
-	props: ["categories", "category", "baseurl", "main"]
+	props: ["categories", "category", "baseurl", "main"],
+	mounted() {
+		const categories = document.querySelector("#categories");
+		const active = categories.querySelector(".nuxt-link-exact-active");
+		/* console.log(active);
+		const leftPos = active.offsetLeft;
+		categories.scrollLeft = leftPos; */
+		active.scrollIntoView({behavior: "smooth", inline: "center"});
+	}
 }
 </script>
