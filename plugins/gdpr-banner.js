70479998c637abad3d7e6ea50f8277ec2edea6ce
Anatoliy Osypenko (DreaMinder) <dreaminder7@gmail.com>
2020-11-23 14:56:54 +0200

    fix gdpr

--- plugins/gdpr-banner.js
+++ plugins/gdpr-banner.js
@@ -1,7 +1,7 @@
 import VS2 from 'vue-script2'
 
 export default ({ env }) => {
-  if (window.location.host !== 'www.coreis.it') return
+  // if (window.location.host !== 'ihei.it') return // TODO: fix host
   
   window._iub = window._iub || [];
   window._iub.csConfiguration = {"siteId":2072615,"lang":"it","cookiePolicyId":39983958, "banner":{ "position":"float-top-center","rejectButtonColor":"#0073CE","rejectButtonCaptionColor":"white","textColor":"black","backgroundColor":"white","acceptButtonDisplay":true,"acceptButtonColor":"#0073CE","acceptButtonCaptionColor":"white","customizeButtonDisplay":true,"customizeButtonColor":"#DADADA","customizeButtonCaptionColor":"#4D4D4D" }};
